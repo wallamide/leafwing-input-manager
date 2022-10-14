edcd852fbdcbd457b0427f4f5819be2ad486b984
Brian Merchant <bhmerchang@gmail.com>
2022-10-14 06:06:30 -0700

    Bullets.

--- examples/cooldown.rs
+++ examples/cooldown.rs
@@ -2,20 +2,26 @@
 //!
 //! This example shows off a tiny jump-move-and-shoot platformer!
 use bevy::core_pipeline::clear_color::ClearColorConfig;
+use bevy::math::Vec3Swizzles;
 use bevy::prelude::*;
+use bevy::sprite::collide_aabb;
 use leafwing_input_manager::prelude::*;
+use std::time::Duration;
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .add_plugin(InputManagerPlugin::<PlayerAction>::default())
+        .insert_resource(PlayerTouchingPlatform::new(true))
         .add_startup_system(spawn_camera)
         .add_startup_system(spawn_platforms)
         .add_startup_system(spawn_player)
         .add_startup_system(spawn_blobs)
-        .add_system(player_act)
-        .add_system(player_gravity)
+        .add_system(update_player_contact)
+        .add_system(player_act.after(update_player_contact))
+        .add_system(player_move.after(player_act))
         .add_system(despawn_blob)
+        .add_system(bullet_move)
         .add_system(despawn_bullets)
         .run();
 }
@@ -50,7 +56,7 @@ struct PlatformBundle {
 }
 
 const PLATFORM_SIZE: Vec2 = Vec2::new(300.0, 30.0);
-const PLATFORM_GAP: Vec2 = Vec2::new(PLATFORM_SIZE.x + 50.0, 0.0);
+const PLATFORM_GAP: Vec2 = Vec2::new(PLATFORM_SIZE.x + 50.0, 50.0);
 
 pub fn spawn_platforms(mut commands: Commands) {
     commands.spawn_bundle(PlatformBundle {
@@ -79,13 +85,27 @@ pub fn spawn_platforms(mut commands: Commands) {
     });
 }
 
+struct PlayerTouchingPlatform(bool);
+
+impl PlayerTouchingPlatform {
+    pub fn new(is_touching: bool) -> PlayerTouchingPlatform {
+        PlayerTouchingPlatform(is_touching)
+    }
+
+    pub fn is_touching(&self) -> bool {
+        self.0
+    }
+
+    pub fn set_touching(&mut self, is_touching: bool) {
+        self.0 = is_touching;
+    }
+}
+
 #[derive(Component, Default)]
 struct Player;
 
-// Keeping things simple at the start, we only track the player's y-velocity.
-// Later we can make this a Vec2.
 #[derive(Component, Default)]
-struct Velocity(f32);
+struct Velocity(Vec2);
 
 #[derive(Bundle, Default)]
 struct PlayerBundle {
@@ -98,6 +118,7 @@ struct PlayerBundle {
 }
 
 const PLAYER_SIZE: Vec2 = Vec2::new(25.0, 25.0);
+const MAX_PLAYER_VELOCITY: Vec2 = Vec2::new(25.0, 25.0);
 
 pub fn spawn_player(mut commands: Commands) {
     commands.spawn_bundle(PlayerBundle {
@@ -118,54 +139,145 @@ pub fn spawn_player(mut commands: Commands) {
                 (KeyCode::W, PlayerAction::Jump),
                 (KeyCode::A, PlayerAction::MoveLeft),
                 (KeyCode::D, PlayerAction::MoveRight),
-                (KeyCode::S, PlayerAction::Shoot),
+                (KeyCode::Space, PlayerAction::Shoot),
             ]),
         },
         ..default()
     });
 }
 
+pub const PLAYER_RUN_MAGNITUDE: f32 = 10.0;
+
 fn player_act(
-    mut player_transform_query: Query<&mut Transform, With<Player>>,
+    mut commands: Commands,
+    mut velocity_query: Query<(&Transform, &mut Velocity), With<Player>>,
     action_query: Query<&ActionState<PlayerAction>, With<Player>>,
+    player_touching_platform: Res<PlayerTouchingPlatform>,
 ) {
     let action_state = action_query.single();
+    let (player_transform, mut velocity) = velocity_query.single_mut();
 
-    if action_state.just_pressed(PlayerAction::Jump) {}
-
-    if action_state.just_pressed(PlayerAction::Shoot) {
-        // bullet spawning happens here
+    if action_state.just_pressed(PlayerAction::Jump) {
+        velocity.0.y += 15.0;
     }
 
-    if action_state.pressed(PlayerAction::MoveLeft) {
-        let mut player_transform = player_transform_query.single_mut();
-        player_transform.translation.x -= 1.0;
+    if action_state.pressed(PlayerAction::Shoot) {
+        commands.spawn_bundle(BulletBundle::new(player_transform.translation.xy()));
     }
 
-    if action_state.pressed(PlayerAction::MoveRight) {
-        let mut player_transform = player_transform_query.single_mut();
-        player_transform.translation.x += 1.0;
+    if action_state.pressed(PlayerAction::MoveLeft) && player_touching_platform.is_touching() {
+        velocity.0.x -= PLAYER_RUN_MAGNITUDE;
+    }
+
+    if action_state.pressed(PlayerAction::MoveRight) && player_touching_platform.is_touching() {
+        velocity.0.x += PLAYER_RUN_MAGNITUDE;
     }
 }
 
-fn player_gravity(mut player_transform_query: Query<&mut Transform, With<Player>>) {
-    let mut player_transform = player_transform_query.single_mut();
+fn update_player_contact(
+    mut player_query: Query<(&mut Transform, &Sprite), With<Player>>,
+    platform_query: Query<(&Transform, &Sprite), (With<Platform>, Without<Player>)>,
+    mut player_touching_platform: ResMut<PlayerTouchingPlatform>,
+) {
+    let (mut player_transform, player_sprite) = player_query.single_mut();
+
+    let player_size = player_sprite.custom_size.unwrap();
+    let player_pos = player_transform.translation;
+
+    let mut is_touching = false;
+    for (platform_transform, platform_sprite) in platform_query.iter() {
+        let platform_size = platform_sprite.custom_size.unwrap();
+        if collide_aabb::collide(
+            player_pos,
+            player_size,
+            platform_transform.translation,
+            platform_size,
+        )
+        .is_some()
+        {
+            is_touching = true;
+            player_transform.translation.y =
+                platform_transform.translation.y + (platform_size.y / 2.0) + (player_size.y / 2.0);
+            break;
+        }
+    }
+    player_touching_platform.set_touching(is_touching);
+}
+
+pub const PLATFORM_FRICTION: f32 = 0.8;
+pub const GRAVITY: f32 = 1.0;
+
+fn player_move(
+    mut player_query: Query<(&mut Velocity, &mut Transform), With<Player>>,
+    player_touching_platform: Res<PlayerTouchingPlatform>,
+) {
+    let (mut player_velocity, mut player_transform) = player_query.single_mut();
+
+    if player_touching_platform.is_touching() {
+        player_velocity.0.x += -1.0 * player_velocity.0.x * PLATFORM_FRICTION;
+        if player_velocity.0.y < 0.0 {
+            player_velocity.0.y = 0.0;
+        }
+    } else {
+        player_velocity.0.y -= GRAVITY;
+    }
+
+    player_transform.translation += player_velocity.0.extend(0.0);
 }
 
 #[derive(Component, Default)]
 struct Bullet;
+#[derive(Component)]
+struct BulletTimer(Timer);
 
-#[derive(Bundle, Default)]
+#[derive(Bundle)]
 struct BulletBundle {
-    blob: Blob,
+    bullet: Bullet,
     #[bundle]
     sprite_bundle: SpriteBundle,
+    velocity: Velocity,
+    timer: BulletTimer,
+}
+
+const BULLET_VELOCITY: Vec2 = Vec2::new(15.0, 0.0);
+
+impl BulletBundle {
+    pub fn new(start_position: Vec2) -> BulletBundle {
+        let timer = Timer::new(Duration::from_millis(500), false);
+        BulletBundle {
+            bullet: Bullet,
+            sprite_bundle: SpriteBundle {
+                sprite: Sprite {
+                    color: Color::GRAY,
+                    custom_size: Some(BULLET_SIZE),
+                    ..default()
+                },
+                transform: Transform::from_translation(start_position.extend(0.0)),
+                ..default()
+            },
+            velocity: Velocity(BULLET_VELOCITY),
+            timer: BulletTimer(timer),
+        }
+    }
 }
 
 const BULLET_SIZE: Vec2 = Vec2::new(3.0, 2.0);
 
-fn despawn_bullets() {
-    // bullets should be despawned after N seconds
+fn bullet_move(mut bullet_query: Query<(&mut Velocity, &mut Transform), With<Bullet>>) {
+    for (v, mut t) in bullet_query.iter_mut() {
+        t.translation += v.0.extend(0.0);
+    }
+}
+
+fn despawn_bullets(
+    mut commands: Commands,
+    bullet_query: Query<(Entity, &BulletTimer), With<Bullet>>,
+) {
+    for (e, t) in bullet_query.iter() {
+        if t.0.finished() {
+            commands.entity(e).despawn();
+        }
+    }
 }
 
 #[derive(Component, Default)]
